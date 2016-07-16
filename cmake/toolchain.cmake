354400acc9aa84321ff6ca4684be15b344cee7c3
Kuba Sejdak <kuba.sejdak@gmail.com>
2016-07-16 22:32:41 +0200

    Refactor setting toolchain in cmake.

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -5,25 +5,31 @@ macro(set_toolchain ARCH)
         set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
         set(CMAKE_ASM_COMPILER "arm-none-eabi-as")
 
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mcpu=cortex-m4 -march=armv7e-m -mthumb -mfpu=fpv4-sp-d16 -fno-common -ffunction-sections -fdata-sections -static -lgcc")
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostdlib -fno-exceptions -fno-unwind-tables -fno-rtti")
-        #set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -nostdinc")                 # Do not include standard headers.
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-threadsafe-statics")   # Do not produce code to initialize statics in thread-safe way.
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS}")
+        set(COMMON_FLAGS "-mcpu=cortex-a8 -march=armv7-a -mthumb -mfpu=neon -mfloat-abi=hard -fno-common -ffunction-sections -fdata-sections -static -lgcc -Wall -Wextra -nostartfiles -nostdinc -nostdlib")
         set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
         set(CMAKE_EXECUTABLE_SUFFIX ".elf")
 
         # Set linker script.
-        set(CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} "-T ${COSMOS_BOARD_PATH}/${COSMOS_TARGET}.ld")
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${COSMOS_BOARD_PATH}/${COSMOS_TARGET}.ld")
         set(CMAKE_EXE_LINKER_FLAGS_RELEASE ${CMAKE_EXE_LINKER_FLAGS_RELEASE} "-s")
 
-        # Set compilation flags.
-        set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-std=c++14 -Wall -Wextra")
-        set(CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG} "-O0")
-        set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_RELEASE} "-O2 -Werror")
+        # Set C compilation flags.
+        set(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=c11")
+        set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0")
+        set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2 -Werror")
 
+        # Set C++ compilation flags.
+        set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=c++14")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-exceptions -fno-unwind-tables")    # Disable C++ exceptions support, because it requires libc.
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-rtti")                             # Disable C++ run time information, because it requires libc.
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-threadsafe-statics")               # Do not produce code to initialize statics in thread-safe way.
+        set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0")
+        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O2 -Werror")
     endif()
 
+    string(REGEX REPLACE ";" " " CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+    string(REGEX REPLACE ";" " " CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")
+    string(REGEX REPLACE ";" " " CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE}")
     string(REGEX REPLACE ";" " " CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
     string(REGEX REPLACE ";" " " CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
     string(REGEX REPLACE ";" " " CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}")
@@ -31,8 +37,9 @@ macro(set_toolchain ARCH)
     string(REGEX REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE}")
     
     message(STATUS "Setting toolchain to:")
-    message(STATUS "  C compiler     : ${CMAKE_C_COMPILER}")
-    message(STATUS "  C++ compiler   : ${CMAKE_CXX_COMPILER}")
-    message(STATUS "  assembler      : ${CMAKE_ASM_COMPILER}")
-    message(STATUS "  compiler flags : ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+    message(STATUS "  C compiler         : ${CMAKE_C_COMPILER}")
+    message(STATUS "  C++ compiler       : ${CMAKE_CXX_COMPILER}")
+    message(STATUS "  assembler          : ${CMAKE_ASM_COMPILER}")
+    message(STATUS "  C compiler flags   : ${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}")
+    message(STATUS "  C++ compiler flags : ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
 endmacro()
