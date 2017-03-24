e0b6804fb1a921eda3acfa4f9e1b891ff81d64b0
Kuba Sejdak <kuba@chimera.local>
2017-03-24 18:42:02 +0100

    scripts: Fix toolchain settings for new compiler on macOS.

--- scripts/toolchain.cmake
+++ scripts/toolchain.cmake
@@ -4,14 +4,12 @@ macro(set_toolchain ARCH)
         set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
         set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
         set(CMAKE_ASM_COMPILER "arm-none-eabi-as")
+        set(CMAKE_RANLIB "arm-none-eabi-ranlib")
+        set(CMAKE_AR "arm-none-eabi-ar")
 
         set(COMMON_FLAGS "-mcpu=cortex-a8 -march=armv7-a -mthumb -mfpu=neon -mfloat-abi=hard -fno-common -ffunction-sections -fdata-sections -static -Wall -Wextra -nostartfiles -nostdinc")
         set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS)
         set(CMAKE_EXECUTABLE_SUFFIX ".elf")
-
-        # Set linker script.
-        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${COSMOS_BOARD_PATH}/${COSMOS_TARGET}/${COSMOS_TARGET}.ld")
-        set(CMAKE_EXE_LINKER_FLAGS_RELEASE ${CMAKE_EXE_LINKER_FLAGS_RELEASE} "-s")
 
         # Set C compilation flags.
         set(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=c11")
@@ -26,6 +24,17 @@ macro(set_toolchain ARCH)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fcheck-new")                           # Allow returning nullptr from operator new.
         set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0")
         set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O2 -Werror")
+
+        # Set linker flags.
+        if (APPLE)
+            # On macOS -search_paths_first linker flag breaks the build.
+            string(REPLACE "-Wl,-search_paths_first" "" CMAKE_C_LINK_FLAGS ${CMAKE_C_LINK_FLAGS} )
+            string(REPLACE "-Wl,-search_paths_first" "" CMAKE_CXX_LINK_FLAGS ${CMAKE_CXX_LINK_FLAGS} )
+        endif()
+
+        # Set linker flags for creating executable.
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${COSMOS_BOARD_PATH}/${COSMOS_TARGET}/${COSMOS_TARGET}.ld")
+        set(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} -s")
     endif()
 
     string(REGEX REPLACE ";" " " CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
@@ -34,13 +43,20 @@ macro(set_toolchain ARCH)
     string(REGEX REPLACE ";" " " CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
     string(REGEX REPLACE ";" " " CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
     string(REGEX REPLACE ";" " " CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}")
+    string(REGEX REPLACE ";" " " CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS}")
+    string(REGEX REPLACE ";" " " CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS}")
     string(REGEX REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}")
     string(REGEX REPLACE ";" " " CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE}")
     
     message(STATUS "Setting toolchain to:")
-    message(STATUS "  C compiler         : ${CMAKE_C_COMPILER}")
-    message(STATUS "  C++ compiler       : ${CMAKE_CXX_COMPILER}")
-    message(STATUS "  assembler          : ${CMAKE_ASM_COMPILER}")
-    message(STATUS "  C compiler flags   : ${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}")
-    message(STATUS "  C++ compiler flags : ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+    message(STATUS "  C compiler        : ${CMAKE_C_COMPILER}")
+    message(STATUS "  C++ compiler      : ${CMAKE_CXX_COMPILER}")
+    message(STATUS "  assembler         : ${CMAKE_ASM_COMPILER}")
+    message(STATUS "  ranlib            : ${CMAKE_RANLIB}")
+    message(STATUS "  ar                : ${CMAKE_AR}")
+    message(STATUS "  CFLAGS            : ${CMAKE_C_FLAGS} ${CMAKE_C_FLAGS_DEBUG}")
+    message(STATUS "  CXXFLAGS          : ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+    message(STATUS "  C LDFLAGS         : ${CMAKE_C_LINK_FLAGS}")
+    message(STATUS "  C++ LDFLAGS       : ${CMAKE_CXX_LINK_FLAGS}")
+    message(STATUS "  C++ LDFLAGS (exe) : ${CMAKE_EXE_LINKER_FLAGS}")
 endmacro()
